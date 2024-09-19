813ac11170197fb37391a5eb4232c168f142e136
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-09-19 11:56:01 -0400

    update css

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -13,12 +13,12 @@ export default function Slider() {
       // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
       spaceBetween={50}
-      slidesPerView={3}
-      navigation
-      pagination={{ clickable: true }}
+      slidesPerView={1.2}
+      // navigation
+      // pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
+      // onSwiper={(swiper) => console.log(swiper)}
+      // onSlideChange={() => console.log('slide change')}
     >
       <SwiperSlide>
         <p>
