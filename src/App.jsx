476c971efd93aefcd4853fba8b7fdb982072e41f
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-02 23:57:33 -0400

    address iframe issue

--- src/App.jsx
+++ src/App.jsx
@@ -813,7 +813,7 @@ function App() {
                   <h4>Game</h4>
                   <div id="gameClipDiv">
                     <p>Tap/Click around to move</p>
-                    <iframe src="./src/game/index.html" sandbox="allow-same-origin" frameBorder="0" scrolling="no">
+                    <iframe src="./src/game/index.html"  frameBorder="0" scrolling="no" allow-same-origin>
                     </iframe>
                   </div>
                 </div>
