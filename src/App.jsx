fa16c9cac26203ec2fe2069f9d419e52d87ddb6a
themistogenes <85630327+themistogenes@users.noreply.github.com>
2024-10-03 00:46:40 -0400

    set sandbox attribute on game iframe

--- src/App.jsx
+++ src/App.jsx
@@ -813,7 +813,7 @@ function App() {
                   <h4>Game</h4>
                   <div id="gameClipDiv">
                     <p>Tap/Click around to move</p>
-                    <iframe src="./assets/game/index.html"  frameBorder="0" scrolling="no">
+                    <iframe src="./assets/game/index.html"  frameBorder="0" scrolling="no" sandbox>
                     </iframe>
                   </div>
                 </div>
